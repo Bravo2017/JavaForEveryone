41522ce1d6dcac327169c42997b6a5a966e8b343
Suzanne-Feight <Bearofshadow@aol.com>
2016-06-28 16:53:39 -0500

    Test Commit

---
+++ src/com/nationwide/chapter9/feight/EmptyTestClass.java
@@ -0,0 +1,13 @@
+package com.nationwide.chapter9.feight;
+
+public class EmptyTestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
