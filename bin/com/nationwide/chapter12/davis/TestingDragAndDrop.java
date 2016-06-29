8f10c4da52051982553faee3747ce40deae189de
Kathleen Davis <windsordavis@users.noreply.github.com>
2016-06-29 08:59:17 -0500

    Testing Drag and Drop Chapter 12 with new folder

---
+++ bin/com/nationwide/chapter12/davis/TestingDragAndDrop.java
@@ -0,0 +1,17 @@
+package com.nationwide;
+
+public class TestingDragAndDrop {
+
+	public TestingDragAndDrop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
