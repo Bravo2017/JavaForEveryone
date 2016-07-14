e4eba63944b9a1279a9d05848c3cfe597178a246
Tom Henricksen <tom.henricksen@gmail.com>
2016-07-14 14:53:02 -0500

    Add Nick Derby's code

---
+++ src/com/nationwide/chapter12/derby/CashRegisterTester.java
@@ -0,0 +1,21 @@
+package com.nationwide.chapter12.derby;
+
+/**
+ * This program tests the CashRegister class.
+ */
+public class CashRegisterTester {
+	public static void main(String[] args) {
+		CashRegister register1 = new CashRegister();
+		register1.addItem(1.95);
+		register1.addItem(0.95);
+		register1.addItem(2.50);
+
+		Currency dollar = new Currency(1.0, "Dollar");
+		Currency quarter = new Currency(0.25, "Quarter");
+
+		register1.enterPayment(6, dollar);
+		register1.enterPayment(3, quarter);
+
+		System.out.printf("Change: %.2f\n", register1.giveChange());
+	}
+}
