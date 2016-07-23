6c996b7b05183121187ec778024d4fd9b73b5139
Kathleen Davis <windsordavis@users.noreply.github.com>
2016-07-23 11:49:05 -0500

    Create P14Q3.java
    P14.3

---
+++ src/com/nationwide/chapter14/davis/P14Q3.java
@@ -0,0 +1,56 @@
+package com.nationwide;
+
+// P14.3 Write a program that automatically 
+// generates the table of sample run times for 
+// the selection sort algorithm. 
+// The program should ask for the smallest and largest 
+// value of n and the number of measurements 
+// and then make all sample runs. 
+// using System.currentTimeMillis
+
+/**
+ * @author DAVISK51
+ * 
+ */
+public class P14Q3 {
+
+	private long elapsedTime;
+	private long startTime;
+	private boolean isRunning;
+
+	public P14Q3() {
+		reset();
+
+	}
+
+	public void start() {
+		if (isRunning) {
+			return;
+		}
+		isRunning = true;
+		startTime = System.currentTimeMillis();
+	}
+
+	public void stop() {
+		if (!isRunning) {
+			return;
+		}
+		isRunning = false;
+		long endTime = System.currentTimeMillis();
+		elapsedTime = elapsedTime + endTime - startTime;
+	}
+
+	public long getElapsedTime() {
+		if (isRunning) {
+			long endTime = System.currentTimeMillis();
+			return elapsedTime + endTime - startTime;
+		} else {
+			return elapsedTime;
+		}
+	}
+
+	public void reset() {
+		elapsedTime = 0;
+		isRunning = false;
+	}
+}
