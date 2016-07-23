ecc521b4ff4c4959e28f5ab7d3f1aa2f0f3f3500
Kathleen Davis <windsordavis@users.noreply.github.com>
2016-07-23 13:37:00 -0500

    Update P14Q2SortDemo.java

--- src/com/nationwide/chapter14/davis/P14Q2SortDemo.java
+++ src/com/nationwide/chapter14/davis/P14Q2SortDemo.java
@@ -9,15 +9,19 @@ import java.util.Arrays;
 
 public class P14Q2SortDemo {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		P14Q2Sort();
+	}
+
+	public static void P14Q2Sort() {
 
 		double[] coins = P14Q2ArrayUtil.fillArray();
 		System.out.print("Array before : ");
 		System.out.println(Arrays.toString(coins));
-
 		P14Q2Sort.sort(coins);
 		System.out.print("Array after  : ");
 		System.out.println(Arrays.toString(coins));
 
 	}
+
 }
