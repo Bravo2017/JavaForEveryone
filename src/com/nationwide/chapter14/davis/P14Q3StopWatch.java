14bced07fbcc0e5e2eb93228d382f502774e95e1
Kathleen Davis <windsordavis@users.noreply.github.com>
2016-07-23 13:35:31 -0500

    Update P14Q3StopWatch.java

--- src/com/nationwide/chapter14/davis/P14Q3StopWatch.java
+++ src/com/nationwide/chapter14/davis/P14Q3StopWatch.java
@@ -8,7 +8,10 @@ public class P14Q3StopWatch {
 
 		P14Q3 timer = new P14Q3();
 		timer.start();
-		P14Q2SortDemo p14q2 = new P14Q2SortDemo();
+		P14Q2SortDemo p14q00 = new P14Q2SortDemo();
+		p14q00.P14Q2Sort();
+		p14q00.P14Q2Sort();
+		p14q00.P14Q2Sort();
 		timer.stop();
 
 		System.out.println("Elapsed time: " + timer.getElapsedTime()
