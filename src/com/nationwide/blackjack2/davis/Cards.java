a0b4d807f0ee7221cabc72dc50ab28ef8d8df66d
Kathleen Davis <windsordavis@users.noreply.github.com>
2016-07-27 19:58:25 -0500

    Create Cards.java

---
+++ src/com/nationwide/blackjack2/davis/Cards.java
@@ -0,0 +1,11 @@
+package com.nationwide;
+
+public class Cards {
+	String cardName;
+	String cardSuit;
+	int cardPoints;
+
+	public Cards(String cardName, String cardSuit, int cardPoints) { // constructor
+	}
+
+}
